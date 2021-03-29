7becc5cd04d0e545a16bb3bec233bcdf232ee465
Carly Sandler <carly.sandler@gmail.com>
2021-03-29 19:54:04 -0400

    Uploading retirement_info file and query code.

--- schema.sql
+++ schema.sql
@@ -2,9 +2,9 @@
 CREATE TABLE departments (
 	dept_no VARCHAR(4) NOT NULL,
 	dept_name VARCHAR(40) NOT NULL,
-	PRIMARY KEY (dept_no)
+	PRIMARY KEY (dept_no),
+	UNIQUE (dept_name)
 );
-
 
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
@@ -15,13 +15,14 @@ CREATE TABLE employees (
 	hire_date DATE NOT NULL,
 	PRIMARY KEY(emp_no)
 );
+
 CREATE TABLE dept_manager (
 	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
 
@@ -40,7 +41,7 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
 );
 
 CREATE TABLE dept_emp (
@@ -52,5 +53,3 @@ CREATE TABLE dept_emp (
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
-
-SELECT * FROM departments;
