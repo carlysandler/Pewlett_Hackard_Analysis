779e5df8767a6ea4b6e9f0656b43d9914c8254b8
Carly Sandler <carly.sandler@gmail.com>
2021-03-30 20:33:16 -0400

    updating queries.sql with three new queries.

--- queries.sql
+++ queries.sql
@@ -56,3 +56,109 @@ WHERE de.to_date = ('9999-01-01');
 
 -- New table: current employees who are retirement eligible
 SELECT * FROM current_emp;
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- Create a new table, then export it as a CSV
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO dept_current_emp
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- Final Table: dept_current_emp (current employee count by department number)
+SELECT * FROM dept_current_emp;
+SELECT * FROM dept_manager;
+
+-- 7.3.5 Create Additional Lists
+-- Requested three more lists (more specific)
+-- 1. Employee Info : list of employees w/:
+	-- unique emp_no,
+	-- last_name, first_name, 
+	-- gender, salary
+-- 2. Management : list of dept_managers w/:
+	-- unique dept_no, dept_name
+	-- emp_no
+	-- last_name, first_name, 
+	-- hire_date, to_date
+-- 3. Department Retirees: updated current_emp w/:
+	-- emp_no (already have),
+	-- first_name, last_name (already_have), 
+	-- to_ date (already have)
+	-- (+) dept_no, dept_name
+
+--- List 1: Employee Information
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date 
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1955-01-01' AND '1955-12-31') 
+	AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	AND (de.to_date = '9999-01-01');
+
+SELECT * FROM emp_info;
+
+-- List 2: Management [3 tables needed]
+-- dept_name (departments),on pk = dept_no  
+-- first_name, last_name (employees)
+-- from_date, to_date (managers)
+
+-- List of managers per department
+SELECT dm.dept_no,
+		d.dept_name,
+		dm.emp_no,
+		ce.last_name,
+		ce.first_name,
+		dm.from_date,
+		dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+	INNER JOIN departments AS d
+		ON (dm.dept_no = d.dept_no)
+	INNER JOIN current_emp AS ce
+		ON (dm.emp_no = ce.emp_no);
+
+-- List 3: Department Retirees
+-- Using Departments & Dept_Emp
+-- use inner joins on: current_emp, departments, and dept_emp
+-- 1. emp_no
+-- 2. first_name
+-- 3. last_name
+-- 4. dept_name
+
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+-- INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp as de
+	ON (ce.emp_no = de.emp_no)
+INNER JOIN departments as d
+	ON (de.dept_no = d.dept_no);
+
+-- 1. What's going on with the salaries?
+-- 2. Why are there only five active managers for nine departments?
+-- 3. Why are some employees appearing twice?
+
+
+
+
+
+
