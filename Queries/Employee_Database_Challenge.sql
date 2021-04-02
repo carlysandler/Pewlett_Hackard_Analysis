6a3317546fe7f685389537929c4c790975be2696
Carly Sandler <carly.sandler@gmail.com>
2021-04-02 11:07:31 -0400

    Uploading Deliverable 1 Challenge.

---
+++ Queries/Employee_Database_Challenge.sql
@@ -0,0 +1,37 @@
+-- DELIVERABLE 1: Number of Retiring Employees by Title
+
+-- (I) Create Retirement Titles CSV file
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+-- Confirm table creation accuracy
+SELECT * FROM retirement_titles;
+
+-- (II) Use DISTINCT ON with ORDER BY to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+-- Confirm table
+SELECT * FROM unique_titles;
+
+-- (III) Retrieve the number of employees (COUNT on unique job titles)
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT DESC;
+-- Confirm table
+SELECT * FROM retiring_titles;
