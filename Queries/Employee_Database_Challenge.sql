7534b17c656ccac8ca812095a3140a4ec8fb9d41
Carly Sandler <carly.sandler@gmail.com>
2021-04-02 16:40:18 -0400

    uploading written summary images (Resources folder).

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -13,7 +13,7 @@ INNER JOIN titles as t
 ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
--- Confirm table quer
+-- Confirm table query
 SELECT * FROM retirement_titles;
 
 -- (II) Use DISTINCT ON with ORDER BY to remove duplicate rows
@@ -52,7 +52,7 @@ INNER JOIN dept_emp as de
 ON(e.emp_no = de.emp_no)
 INNER JOIN titles as t
 ON(e.emp_no = t.emp_no)
-WHERE (birth_date BETWEEN '1965-01-01'AND '1965-12-31')
+WHERE (e.birth_date BETWEEN '1965-01-01'AND '1965-12-31')
 AND (de.to_date = '9999-01-01')
 ORDER BY e.emp_no;
 -- Confirm table query
